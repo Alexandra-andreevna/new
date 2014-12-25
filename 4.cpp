a8fa3e7e78957bb09d0990c15f892f8d418813f9
Alexandra-andreevna <Alexandra25andreevna@gmail.com>
2014-12-25 12:30:14 +0400

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -3,32 +3,15 @@
 #include <string>
 #include <fstream>
 using namespace std;
-int pos (string a, string b)
- {
-	int m,l,k,d;
-	m=a.length();
-	l=b.length();
-	for (int i=1; i<m; i++)
-	{
-		if (a[i]==b[1])
-		for (int j=1; j<l; j++)
-			if (a[i+j-1]==b[j])
-				k=k+1;
-		if (k==i)
-			return d;
- }
-}
-
 int main() 
 {
 	ifstream in;
-	ifstream out;
 	ofstream out;
 	in.open("input.txt");
 	out.open("output.txt");
 	//enter line
 	string s, str;
-	getline (out,s);
+	getline (in,s);
 	str.append(s);
 	str.append("\n");
 	int l,n,k;
@@ -36,17 +19,19 @@ int main()
    {
 		//enter abbreviation and decryption
 		string a,d,abb,dec;
-		getline (in,a," ");
+		getline (in,a);
 		abb.append(a);
 		getline (in,d);
 		dec.append(d);
 		l=abb.length();
 		n=dec.length();
 		k=0;
-		k=pos(str,abb)
+		k=str.find(abb);
 		if (k!=0)
+		{
 			str.erase(k,l);
-		str.insert(k,dec,1,n);
+			str.insert(k,dec,1,n);
+		};
 	};
 	out<<str;
 	in.close();
